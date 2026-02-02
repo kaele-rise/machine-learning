1a7240f1bd104f480e1c79e20ebf95073e4355a9
kaele-rise <xodorov06@gmail.com>
2026-02-02 21:33:54 +1000

    release regressor

--- AdaBoost/AdaBoostRegressor/AdaBoostRegressor.py
+++ AdaBoost/AdaBoostRegressor/AdaBoostRegressor.py
@@ -5,9 +5,34 @@ from sklearn.tree import DecisionTreeRegressor
 train_data = pd.read_csv('train.csv')
 test_data = pd.read_csv('test.csv')
 
-x_train = train_data.drop(['id', 'SMILES', 'Tm'], axis=1)
-y_train = train_data['Tm']
+x_train = np.array(train_data.drop(['id', 'SMILES', 'Tm'], axis=1))
+y_train = np.array(train_data['Tm']).T
 
-x_test = test_data.drop(['id', 'SMILES'], axis=1)
+x_test = np.array(test_data.drop(['id', 'SMILES'], axis=1))
+
+
+T = 12
+max_depth = 3
+algs = []
+s = np.array(y_train.ravel())
+
+for t in range(T):
+    algs.append(DecisionTreeRegressor(max_depth=max_depth))
+    algs[t].fit(x_train, s)
+    s -= algs[t].predict(x_train)
+
+
+y_test = algs[0].predict(x_test)
+for i in range(1, T):
+    y_test += algs[i].predict(x_test)
+
+prediction = pd.DataFrame({'id': np.array(test_data['id']),
+                           'Tm': y_test})
+
+print(prediction.head())
+
+prediction.to_csv('prediction.csv', index=False, encoding='utf-8')
+
+
 
 
