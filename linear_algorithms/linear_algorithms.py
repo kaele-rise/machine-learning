6c48cf69c9e555377b9f8d972acccb8a74a6b144
kaele-rise <xodorov06@gmail.com>
2026-02-21 21:20:46 +1000

    main algo

--- linear_algorithms/linear_algorithms.py
+++ linear_algorithms/linear_algorithms.py
@@ -3,8 +3,12 @@ import pandas as pd
 import numpy as np
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
+from sklearn.linear_model import ElasticNet, Lasso, Ridge, LinearRegression
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, r2_score
+
 
 test_data = pd.read_csv('test.csv')
 train_data = pd.read_csv('train.csv')
@@ -17,9 +21,13 @@ cat_columns = ['Sex', 'HasPartner', 'HasChild', 'HasPhoneService',
 
 num_columns = ['ClientPeriod', 'MonthlySpending', 'TotalSpent', 'IsSeniorCitizen']
 
-x_train = np.array(train_data.drop('Churn', axis=1))
-y_train = np.array(train_data['Churn'])
-x_test = np.array(test_data)
+for col in num_columns:
+    train_data[col] = pd.to_numeric(train_data[col], errors='coerce')
+    test_data[col] = pd.to_numeric(test_data[col], errors='coerce')
+
+x_train = train_data.drop('Churn', axis=1)
+y_train = train_data['Churn']
+x_test = test_data
 
 
 
@@ -40,6 +48,34 @@ preprocessor = ColumnTransformer([
 ])
 
 
+models = {
+    'Linear Regression': LinearRegression(),
+    'Ridge': Ridge(alpha=1.0),
+    'Lasso': Lasso(alpha=0.1),
+    'ElasticNet': ElasticNet(alpha=0.1, l1_ratio=0.5, random_state=42),
+}
 
+x_train_, x_test_, y_train_, y_test_ = train_test_split(x_train, y_train, test_size=0.2, random_state=42)
+
+
+predictions = {}
+metrics = {}
+
+for name, model in models.items():
+    pipeline = Pipeline([
+        ('preprocessor', preprocessor),
+        ('model', model)
+    ])
+    pipeline.fit(x_train_, y_train_)
+
+    y_pred_ = pipeline.predict(x_test_)
+    predictions[name] = y_pred_
+
+    mse = mean_squared_error(y_test_, y_pred_)
+    r2 = r2_score(y_test_, y_pred_)
+    metrics[name] = {'MSE': mse, 'R2': r2}
+
+
+print(metrics)
 
 
