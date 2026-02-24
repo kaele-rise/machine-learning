bf30ae433c3b3a41560e9c3639a7a151c0921fee
kaele-rise <xodorov06@gmail.com>
2026-02-24 14:42:08 +1000

    added classification comparsion

--- linear_algorithms/linear_algorithms.py
+++ linear_algorithms/linear_algorithms.py
@@ -1,14 +1,15 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+from sklearn.calibration import CalibratedClassifierCV
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
-from sklearn.linear_model import ElasticNet, Lasso, Ridge, LinearRegression
+from sklearn.linear_model import LogisticRegression, Perceptron, RidgeClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error, r2_score
-
+from sklearn.metrics import f1_score, roc_auc_score
 
 test_data = pd.read_csv('test.csv')
 train_data = pd.read_csv('train.csv')
@@ -49,10 +50,10 @@ preprocessor = ColumnTransformer([
 
 
 models = {
-    'Linear Regression': LinearRegression(),
-    'Ridge': Ridge(alpha=1.0),
-    'Lasso': Lasso(alpha=0.1),
-    'ElasticNet': ElasticNet(alpha=0.1, l1_ratio=0.5, random_state=42),
+    'Linear Regression': LogisticRegression(),
+    'Ridge': Perceptron(),
+    'Lasso': RidgeClassifier(),
+    'ElasticNet': LinearDiscriminantAnalysis(),
 }
 
 x_train_, x_test_, y_train_, y_test_ = train_test_split(x_train, y_train, test_size=0.2, random_state=42)
@@ -60,6 +61,7 @@ x_train_, x_test_, y_train_, y_test_ = train_test_split(x_train, y_train, test_s
 
 predictions = {}
 metrics = {}
+
 
 for name, model in models.items():
     pipeline = Pipeline([
@@ -71,11 +73,38 @@ for name, model in models.items():
     y_pred_ = pipeline.predict(x_test_)
     predictions[name] = y_pred_
 
-    mse = mean_squared_error(y_test_, y_pred_)
-    r2 = r2_score(y_test_, y_pred_)
-    metrics[name] = {'MSE': mse, 'R2': r2}
+    model_metrics = {}
+    if hasattr(model, 'predict_proba'):
+        auc = roc_auc_score(y_test_, pipeline.predict_proba(x_test_)[:, 1])
+    else:
+        calibrated = CalibratedClassifierCV(pipeline, method='sigmoid', cv=5)
+        calibrated.fit(x_train_, y_train_)
+        prob = calibrated.predict_proba(x_test_)
+        auc = roc_auc_score(y_test_, prob[:, 1])
+
+    model_metrics['AUC-ROC'] = auc
+    f1 = f1_score(y_test_, y_pred_, average='macro')
+    model_metrics['F1'] = f1
+    metrics[name] = model_metrics
 
 
 print(metrics)
+
+
+# prediction_pipeline = Pipeline([
+#     ('preprocessor', preprocessor),
+#     ('model', Ridge(alpha=1.0))
+# ])
+# prediction_pipeline.fit(x_train, y_train)
+# y_pred = prediction_pipeline.predict(x_test)
+#
+# prediction = pd.DataFrame({
+#     'Id': [i for i in range(len(y_pred))],
+#     'Churn': y_pred
+# })
+# prediction.to_csv('prediction.csv', index=False, encoding='utf-8')
+
+
+
 
 
